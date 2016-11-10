dc98434747949d6769426e43340e9aefaa94656c
meg-murphy <meghan.murphy.usc@gmail.com>
2016-11-09 21:29:18 -0800

    add data to seed file & formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(name: "The Cashmerre Crop Mockneck", price: "130", image: "https://everlane-2.imgix.net/i/34d07gfd_4531.jpg?w=1200&h=1200&q=65&dpr=1", description: "A slightly oversized cashmere mockneck with a relaxed but playful cropped cut.")
+product.save
+product = Product.new(name: "The Belted Wool Shawl Coat", price: "250", image: "https://everlane-2.imgix.net/i/909bfa9e_70f2.jpg?w=1200&h=1200&q=65&dpr=1", description: "A beautiful double-faced wool coat with an elongated silhouette that will keep you warm.")
+product.save
+product = Product.new(name: "The Heeled Oxford", price: "180", image: "https://everlane.imgix.net/i/2a426541_1199.jpg?w=1200&h=1200&q=65&dpr=1", description: "A sleek Italian leather Oxford with a comfortable, wide heel.")
+product.save
