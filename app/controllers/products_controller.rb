1414fef570f848530e465327f1735ccfed4b658b
meg-murphy <meghan.murphy.usc@gmail.com>
2016-11-14 21:24:42 -0800

    update routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,35 @@ class ProductsController < ApplicationController
     @page_title = "EVERLANE 2.0"
 
     @products = Product.all
+  end
+
+  def index
+    @page_title = "EVERLANE 2.0"
+
+    @products = Product.all
+  end
+
+  def show
+    @products = Product.find_by(id: params[:id])
+  end
+
+  def new
+  end
 
 
+  def create
+    @products = Product.new(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    @products.save
+  end
+
+  def edit
+    @products = Product.find_by(id: params[:id])
+  end
+
+  def update
+    products = Product.find_by(id: params[:id])
+    products.assign_attributes(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    products.save
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/", to: "products#products_home"
+  get "/all_products", to: "products#index"
+
+  get "all_products/new", to: "products#new"
+  post "all_products", to: "products#create"
+
+  get "/all_products/:id/edit", to: "products#edit"
+  patch "/all_products/:id", to: "products#update"
+
+  get "/all_products/:id", to: "products#show"
+
 
 end
