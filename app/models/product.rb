b4952b59c8250b4c254d2339b7a125ec55af8baa
meg-murphy <meghan.murphy.usc@gmail.com>
2016-11-17 19:52:10 -0800

    add sale price and message. add tax.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ApplicationRecord
 
-  
+  def sale_message
+    if price > "2"
+      return "Discount Item!"
+    else
+      return "On Sale!"
+    end
+  end
+
+  def tax
+    return price.to_i * 0.09
+  end
+
+  def total
+    return price.to_i * 0.09 + price.to_i
+  end
+
 end
